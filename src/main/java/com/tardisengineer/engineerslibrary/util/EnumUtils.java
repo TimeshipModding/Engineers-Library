1261ee49884339a3b298396fcdfa20f4145460d3
TardisEngineer <doctorwho5611@gmail.com>
2023-08-05 23:11:16 +1000

    Added more to VoxelShapeUtils and added EnumUtils

---
+++ src/main/java/com/tardisengineer/engineerslibrary/util/EnumUtils.java
@@ -0,0 +1,13 @@
+package com.tardisengineer.engineerslibrary.util;
+
+import net.minecraft.core.Direction;
+
+public class EnumUtils {
+
+    private EnumUtils() {
+    }
+    public static final Direction[] DIRECTIONS = Direction.values();
+
+    public static final Direction[] HORIZONTAL_DIRECTIONS = {Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST};
+
+}
--- src/main/java/com/tardisengineer/engineerslibrary/util/VoxelShapeUtils.java
+++ src/main/java/com/tardisengineer/engineerslibrary/util/VoxelShapeUtils.java
@@ -1,24 +1,84 @@
 package com.tardisengineer.engineerslibrary.util;
 
 import com.tardisengineer.engineerslibrary.EngineersLibrary;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.UnaryOperator;
 
 public final class VoxelShapeUtils {
+    private static final Vec3 fromOrigin = new Vec3(-0.5, -0.5, -0.5);
+
+    public static AABB rotate(AABB box, Direction side) {
+        return switch (side) {
+            case DOWN -> box;
+            case UP -> new AABB(box.minX, -box.minY, -box.minZ, box.maxX, -box.maxY, -box.maxZ);
+            case NORTH -> new AABB(box.minX, -box.minZ, box.minY, box.maxX, -box.maxZ, box.maxY);
+            case SOUTH -> new AABB(-box.minX, -box.minZ, -box.minY, -box.maxX, -box.maxZ, -box.maxY);
+            case WEST -> new AABB(box.minY, -box.minZ, -box.minX, box.maxY, -box.maxZ, -box.maxX);
+            case EAST -> new AABB(-box.minY, -box.minZ, box.minX, -box.maxY, -box.maxZ, box.maxX);
+        };
+    }
+
+    public static AABB rotate(AABB box, Rotation rotation) {
+        return switch (rotation) {
+            case NONE -> box;
+            case CLOCKWISE_90 -> new AABB(-box.minZ, box.minY, box.minX, -box.maxZ, box.maxY, box.maxX);
+            case CLOCKWISE_180 -> new AABB(-box.minX, box.minY, -box.minZ, -box.maxX, box.maxY, -box.maxZ);
+            case COUNTERCLOCKWISE_90 -> new AABB(box.minZ, box.minY, -box.minX, box.maxZ, box.maxY, -box.maxX);
+        };
+    }
+
+    public static AABB rotateHorizontal(AABB box, Direction side) {
+        return switch (side) {
+            case NORTH -> rotate(box, Rotation.NONE);
+            case SOUTH -> rotate(box, Rotation.CLOCKWISE_180);
+            case WEST -> rotate(box, Rotation.COUNTERCLOCKWISE_90);
+            case EAST -> rotate(box, Rotation.CLOCKWISE_90);
+            default -> box;
+        };
+    }
+
+    public static VoxelShape rotate(VoxelShape shape, Direction side) {
+        return rotate(shape, box -> rotate(box, side));
+    }
+
+    public static VoxelShape rotate(VoxelShape shape, Rotation rotation) {
+        return rotate(shape, box -> rotate(box, rotation));
+    }
+
+    public static VoxelShape rotateHorizontal(VoxelShape shape, Direction side) {
+        return rotate(shape, box -> rotateHorizontal(box, side));
+    }
+
+    public static VoxelShape rotate(VoxelShape shape, UnaryOperator<AABB> rotateFunction) {
+        List<VoxelShape> rotatedPieces = new ArrayList<>();
+        //Explode the voxel shape into bounding boxes
+        List<AABB> sourceBoundingBoxes = shape.toAabbs();
+        //Rotate them and convert them each back into a voxel shape
+        for (AABB sourceBoundingBox : sourceBoundingBoxes) {
+            //Make the bounding box be centered around the middle, and then move it back after rotating
+            rotatedPieces.add(Shapes.create(rotateFunction.apply(sourceBoundingBox.move(fromOrigin.x, fromOrigin.y, fromOrigin.z))
+                    .move(-fromOrigin.x, -fromOrigin.z, -fromOrigin.z)));
+        }
+        //return the recombined rotated voxel shape
+        return combine(rotatedPieces);
+    }
+
     public static VoxelShape combine(VoxelShape... shapes) {
         return batchCombine(Shapes.empty(), BooleanOp.OR, true, shapes);
     }
 
     public static VoxelShape combine(Collection<VoxelShape> shapes) {
         return batchCombine(Shapes.empty(), BooleanOp.OR, true, shapes);
-    }
-
-    public static VoxelShape exclude(VoxelShape... shapes) {
-        return batchCombine(Shapes.block(), BooleanOp.ONLY_FIRST, true, shapes);
     }
 
     public static VoxelShape batchCombine(VoxelShape initial, BooleanOp function, boolean simplify, Collection<VoxelShape> shapes) {
@@ -35,5 +95,20 @@ public final class VoxelShapeUtils {
             combinedShape = Shapes.joinUnoptimized(combinedShape, shape, function);
         }
         return simplify ? combinedShape.optimize() : combinedShape;
+    }
+
+    public static void setShape(VoxelShape shape, VoxelShape[] dest, boolean verticalAxis) {
+        setShape(shape, dest, verticalAxis, false);
+    }
+
+    public static void setShape(VoxelShape shape, VoxelShape[] dest, boolean verticalAxis, boolean invert) {
+        Direction[] dirs = verticalAxis ? EnumUtils.DIRECTIONS : EnumUtils.HORIZONTAL_DIRECTIONS;
+        for (Direction side : dirs) {
+            dest[verticalAxis ? side.ordinal() : side.ordinal() - 2] = verticalAxis ? VoxelShapeUtils.rotate(shape, invert ? side.getOpposite() : side) : VoxelShapeUtils.rotateHorizontal(shape, side);
+        }
+    }
+
+    public static void setShape(VoxelShape shape, VoxelShape[] dest) {
+        setShape(shape, dest, false, false);
     }
 }
